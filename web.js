07f46dc805907c2b5c5254984e379243de9cca8b
alex-33 <alex-33@list.ru>
2013-07-26 18:09:08 +0000

    Added navbar

--- web.js
+++ web.js
@@ -3,6 +3,10 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/img', express.static(__dirname + '/img'));
+app.use('/js', express.static(__dirname + '/js'));
+
 app.get('/', function(request, response) {
     var buffer = fs.readFileSync('index.html');
     response.send(buffer.toString());
